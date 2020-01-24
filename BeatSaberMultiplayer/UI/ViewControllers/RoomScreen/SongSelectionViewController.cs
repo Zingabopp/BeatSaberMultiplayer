09c383ee623a932774abea208cd0d94f394dae7e
Zingabopp <Zingabopp@gmail.com>
2020-01-24 02:38:51 -0600

    Song list state saved and restored when returning to the song list view

--- BeatSaberMultiplayer/UI/ViewControllers/RoomScreen/SongSelectionViewController.cs
+++ BeatSaberMultiplayer/UI/ViewControllers/RoomScreen/SongSelectionViewController.cs
@@ -10,7 +10,6 @@ using BeatSaberMultiplayerLite.UI.FlowCoordinators;
 using BeatSaberMarkupLanguage.ViewControllers;
 using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.Components;
-using IPA.Utilities;
 using BeatSaberMarkupLanguage.Parser;
 
 namespace BeatSaberMultiplayerLite.UI.ViewControllers.RoomScreen
@@ -20,7 +19,9 @@ namespace BeatSaberMultiplayerLite.UI.ViewControllers.RoomScreen
     class SongSelectionViewController : BSMLResourceViewController, TableView.IDataSource
     {
         public override string ResourceName => string.Join(".", GetType().Namespace, GetType().Name);
-
+        IPAUtilities.PropertyAccessor<TableViewScroller, float>.Setter SetScrollPosition = IPAUtilities.PropertyAccessor<TableViewScroller, float>.GetSetter("position");
+        IPAUtilities.FieldAccessor<TableViewScroller, float>.Accessor GetTableViewScrollerTargetPosition = IPAUtilities.FieldAccessor<TableViewScroller, float>.GetAccessor("_targetPosition");
+        IPAUtilities.FieldAccessor<TableView, TableViewScroller>.Accessor GetTableViewScroller = IPAUtilities.FieldAccessor<TableView, TableViewScroller>.GetAccessor("_scroller");
         public RoomFlowCoordinator ParentFlowCoordinator { get; internal set; }
         public event Action<IPreviewBeatmapLevel> SongSelected;
         public event Action<string> SearchPressed;
@@ -64,6 +65,21 @@ namespace BeatSaberMultiplayerLite.UI.ViewControllers.RoomScreen
                 NotifyPropertyChanged();
             }
         }
+
+        private TableViewScroller _songListScroller;
+        public TableViewScroller SongListScroller
+        {
+            get
+            {
+                if (_songListScroller == null)
+                {
+                    _songListScroller = GetTableViewScroller(ref _songsTableView.tableView);
+                    _songListScroller.positionDidChangeEvent += _songListScroller_positionDidChangeEvent;
+                }
+                return _songListScroller;
+            }
+        }
+
         private LevelListTableCell songListTableCellInstance;
         private PlayerDataModelSO _playerDataModel;
         private AdditionalContentModel _additionalContentModel;
@@ -117,16 +133,37 @@ namespace BeatSaberMultiplayerLite.UI.ViewControllers.RoomScreen
 
             _songsTableView.tableView.ReloadData();
             _songsTableView.tableView.ScrollToCellWithIdx(0, TableViewScroller.ScrollPositionType.Beginning, false);
-
             Plugin.log.Debug($"Set list of {levels.Count} levels!");
         }
 
         public void ScrollToLevel(string levelId)
         {
+            if (string.IsNullOrEmpty(levelId))
+                return;
             if (availableSongs.Any(x => x.levelID == levelId))
             {
                 int row = availableSongs.FindIndex(x => x.levelID == levelId);
                 _songsTableView.tableView.ScrollToCellWithIdx(row, TableViewScroller.ScrollPositionType.Beginning, false);
+            }
+        }
+
+        public bool ScrollToPosition(float position)
+        {
+            TableViewScroller scroller = SongListScroller;
+            if (position >= 0 && position <= scroller.scrollableSize)
+            {
+                GetTableViewScrollerTargetPosition(ref scroller) = position;                
+                SetScrollPosition(ref scroller, position);
+                return true;
+            }
+            return false;
+        }
+
+        public void ScrollToIndex(int index, bool animated = false)
+        {
+            if (index >= 0 && index < _songsTableView.tableView.numberOfCells)
+            {
+                _songsTableView.tableView.ScrollToCellWithIdx(index, TableViewScroller.ScrollPositionType.Center, animated);
             }
         }
 
@@ -246,9 +283,15 @@ namespace BeatSaberMultiplayerLite.UI.ViewControllers.RoomScreen
 
             tableCell.SetDataFromLevelAsync(availableSongs[idx], _playerDataModel.playerData.favoritesLevelIds.Contains(availableSongs[idx].levelID));
             tableCell.RefreshAvailabilityAsync(_additionalContentModel, availableSongs[idx].levelID);
-
+            
             tableCell.reuseIdentifier = _songsTableView.reuseIdentifier;
             return tableCell;
+        }
+
+
+        private void _songListScroller_positionDidChangeEvent(TableViewScroller arg1, float arg2)
+        {
+            ParentFlowCoordinator.LastScrollPosition = arg2;
         }
 
         [UIAction("fast-scroll-down-pressed")]
