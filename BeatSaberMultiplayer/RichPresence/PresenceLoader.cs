0b0de6eddea6acadbccb51fc1af583b0a3f168f7
Zingabopp <Zingabopp@gmail.com>
2020-01-29 14:16:58 -0600

    Try/Catches for each Presence load

--- BeatSaberMultiplayer/RichPresence/PresenceLoader.cs
+++ BeatSaberMultiplayer/RichPresence/PresenceLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BeatSaberMultiplayerLite.RichPresence
@@ -9,11 +10,23 @@ namespace BeatSaberMultiplayerLite.RichPresence
         {
             List<IPresenceInstance> loadedPresences = new List<IPresenceInstance>();
             if (IPA.Loader.PluginManager.GetPluginFromId("DiscordCore") != null)
-                loadedPresences.Add(LoadDiscord(modId, modName, modIcon, handleInvites, appid));
+            {
+                IPresenceInstance discordPrescence = LoadDiscord(modId, modName, modIcon, handleInvites, appid);
+                if (discordPrescence != null)
+                    loadedPresences.Add(discordPrescence);
+                else
+                    Plugin.log.Warn($"Discord Presence failed to load, Discord Rich Presence unavailable.");
+            }
             else
                 Plugin.log.Debug($"DiscordCore mod not detected, Discord Rich Presence unavailable.");
             if (Plugin.IsSteam)
-                loadedPresences.Add(LoadSteam(modId, modName, modIcon, handleInvites, appid));
+            {
+                IPresenceInstance steamPresence = LoadSteam();
+                if (steamPresence != null)
+                    loadedPresences.Add(steamPresence);
+                else
+                    Plugin.log.Warn($"Discord Presence failed to load, Discord Rich Presence unavailable.");
+            }
             else
                 Plugin.log.Debug($"Running on Oculus platform, Steam Rich Presence unavailable.");
             return loadedPresences.ToArray();
@@ -22,13 +35,29 @@ namespace BeatSaberMultiplayerLite.RichPresence
         public static IPresenceInstance LoadDiscord(string modId, string modName, Sprite modIcon, bool handleInvites, long appid)
         {
             Plugin.log.Debug($"Loading DiscordPresence");
-            IPresenceInstance discordPresence = new DiscordPresence.DiscordPresence(modId, modName, modIcon, handleInvites, appid);
+            IPresenceInstance discordPresence = null;
+            try
+            {
+                discordPresence = new DiscordPresence.DiscordPresence(modId, modName, modIcon, handleInvites, appid);
+            }
+            catch (Exception ex)
+            {
+                Plugin.log.Debug(ex);
+            }
             return discordPresence;
         }
-        public static IPresenceInstance LoadSteam(string modId, string modName, Sprite modIcon, bool handleInvites, long appid)
+        public static IPresenceInstance LoadSteam()
         {
             Plugin.log.Debug($"Loading SteamPresence");
-            IPresenceInstance steamPresence = new GameObject("SteamPresence").AddComponent<SteamPresence.SteamPresence>();
+            IPresenceInstance steamPresence = null;
+            try
+            {
+                steamPresence = new GameObject("SteamPresence").AddComponent<SteamPresence.SteamPresence>();
+            }
+            catch (Exception ex)
+            {
+                Plugin.log.Debug(ex);
+            }
             return steamPresence;
         }
     }
