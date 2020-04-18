fdbf1e7bdedcf97e2e88702d65c352485e3e1eab
Zingabopp <Zingabopp@gmail.com>
2020-04-18 14:08:06 -0500

    PTT Grip bindings work again

--- BeatSaberMultiplayer/Misc/ControllersHelper.cs
+++ BeatSaberMultiplayer/Misc/ControllersHelper.cs
@@ -1,32 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace BeatSaberMultiplayerLite.Misc
 {
     static class ControllersHelper
     {
-        //TODO: Use Unity new input system
-
         private static bool initialized = false;
-        private static int platform = -1;
-        private static VRPlatformHelper platformHelper;
+        private static InputDevice LeftController;
+        private static InputDevice RightController;
+
+        private static InputDevice GetInputDevice(XRNode node)
+        {
+            var device = InputDevices.GetDeviceAtXRNode(node);
+            if (device != null)
+            {
+                Plugin.log.Debug($"Found {node}: {device.manufacturer}|{device.name}|{device.isValid}");
+            }
+            else
+                Plugin.log.Error($"{node} not found.");
+            return device;
+        }
 
         public static void Init()
         {
-            if(platformHelper == null)
-            {
-                platformHelper = Resources.FindObjectsOfTypeAll<VRPlatformHelper>().FirstOrDefault();
-            }
-
-            if (platformHelper.vrPlatformSDK == VRPlatformHelper.VRPlatformSDK.OpenVR)
-                platform = 0;
-            else if (platformHelper.vrPlatformSDK == VRPlatformHelper.VRPlatformSDK.Oculus)
-                platform = 1;
-            else if (Environment.CommandLine.Contains("fpfc") && platformHelper.vrPlatformSDK == VRPlatformHelper.VRPlatformSDK.Unknown)
-                platform = 2;
-            else
-                platform = -1;
+            LeftController = GetInputDevice(XRNode.LeftHand);
+            RightController = GetInputDevice(XRNode.RightHand);
 
             initialized = true;
         }
@@ -37,19 +38,14 @@ namespace BeatSaberMultiplayerLite.Misc
             {
                 Init();
             }
-            // TODO: Spams error messages (at least for some setups)
-            return false;
-            switch (platform)
+            if (RightController.isValid)
             {
-                case 0:
-                    return OpenVRRightGrip();
-                case 1:
-                    return OculusRightGrip();
-                case 2:
-                    return Input.GetKey(KeyCode.H);
-                default:
-                    return false;
+                if (RightController.TryGetFeatureValue(CommonUsages.gripButton, out bool value))
+                {
+                    return value;
+                }
             }
+            return false;
         }
 
         public static bool GetLeftGrip()
@@ -58,54 +54,15 @@ namespace BeatSaberMultiplayerLite.Misc
             {
                 Init();
             }
-            // TODO: Spams error messages (at least for some setups)
+            if (LeftController.isValid)
+            {
+                if (LeftController.TryGetFeatureValue(CommonUsages.gripButton, out bool value))
+                {
+                    return value;
+                }
+            }
             return false;
-            switch (platform)
-            {
-                case 0:
-                    return OpenVRLeftGrip();
-                case 1:
-                    return OculusLeftGrip();
-                case 2:
-                    return Input.GetKey(KeyCode.H);
-                default:
-                    return false;
-            }
         }
-
-        private static bool OculusRightGrip()
-        {
-            return OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.RTouch) > 0.85f;
-        }
-
-        private static bool OculusLeftGrip()
-        {
-            return OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) > 0.85f;
-        }
-
-        private static int _leftControllerIndex = -1;
-        private static int _rightControllerIndex = -1;
-
-        private static bool OpenVRLeftGrip()
-        {
-            if (_leftControllerIndex == -1)
-            {
-                _leftControllerIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
-            }
-
-            return SteamVR_Controller.Input(_leftControllerIndex).GetPress(SteamVR_Controller.ButtonMask.Grip);
-        }
-
-        private static bool OpenVRRightGrip()
-        {
-            if (_rightControllerIndex == -1)
-            {
-                _rightControllerIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
-            }
-
-            return SteamVR_Controller.Input(_rightControllerIndex).GetPress(SteamVR_Controller.ButtonMask.Grip);
-        }
-
     }
 
 
