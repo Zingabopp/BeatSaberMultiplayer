c4426a0bae3436db3c2062c07c2415ae588efe86
Zingabopp <Zingabopp@gmail.com>
2020-04-20 23:58:54 -0500

    .

--- BeatSaberMultiplayer/Misc/Config.cs
+++ BeatSaberMultiplayer/Misc/Config.cs
@@ -287,8 +287,16 @@ namespace BeatSaberMultiplayerLite
 
     public class Vector3Dummy
     {
+        [SerializedName(nameof(X))]
+        [JsonProperty(PropertyName = nameof(X), Order = 0)]
         public virtual float X { get; set; } = 0f;
+
+        [SerializedName(nameof(Y))]
+        [JsonProperty(PropertyName = nameof(Y), Order = 1)]
         public virtual float Y { get; set; } = 0f;
+
+        [SerializedName(nameof(Z))]
+        [JsonProperty(PropertyName = nameof(Z), Order = 2)]
         public virtual float Z { get; set; } = 0f;
 
         public Vector3Dummy() { }
